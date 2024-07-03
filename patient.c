7dfeaecc5633d9b08fbe16dfb65e877cb34e8bd0
JosiasNJB <84824433+JosiasNJB@users.noreply.github.com>
2024-07-03 11:41:29 -0300

    added start of functions

--- patient.c
+++ patient.c
@@ -8,6 +8,7 @@ struct Patient
     int id;
     char* name;
     struct tm* birthdate;
+    struct tm* arrival;
 };
 
 struct Patient* createPatient(int id, const char* name, struct tm* birthdate, struct tm* arrival)
@@ -16,5 +17,30 @@ struct Patient* createPatient(int id, const char* name, struct tm* birthdate, st
     patient->id = id;
     patient->name = name;
     patient->birthdate = birthdate;
+    patient->arrival = arrival;
+
     return patient;
 };
+
+// Libera a memória alocada para a estrutura do paciente.
+void destroy_patient(struct Patient *patient)
+{
+
+};
+
+int get_patient_id(struct Patient* patient)
+{
+
+};
+
+const char get_patient_name(struct Patient* patient)
+{
+
+};
+
+struct tm* get_patient_birthdate(struct Patient patient)
+{
+
+}
+
+
